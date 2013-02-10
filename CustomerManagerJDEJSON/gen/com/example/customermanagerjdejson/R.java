8c8128641af5b7b640a64f367b3cc0a2b131c89a
fspoliveira <fspoliveira@bitwaysystem.com.br>
2013-02-10 01:39:34 -0200

    Fim Ajuste layout land e portrait

--- CustomerManagerJDEJSON/gen/com/example/customermanagerjdejson/R.java
+++ CustomerManagerJDEJSON/gen/com/example/customermanagerjdejson/R.java
@@ -28,11 +28,11 @@ public final class R {
         public static final int lbl_idCliente=0x7f08000b;
         public static final int rodape=0x7f080000;
         public static final int textView1=0x7f080003;
-        public static final int textView2=0x7f080009;
+        public static final int textView2=0x7f08000a;
         public static final int texto2=0x7f080001;
         public static final int txt_CNPJCPF=0x7f080006;
         public static final int txt_Limite=0x7f080008;
-        public static final int txt_PedidoAberto=0x7f08000a;
+        public static final int txt_PedidoAberto=0x7f080009;
         public static final int txt_idCliente=0x7f080007;
     }
     public static final class layout {
