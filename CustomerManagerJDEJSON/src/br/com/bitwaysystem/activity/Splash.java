a99bc8587a267011113b441de609ef784e60108d
fspoliveira <fspoliveira@bitwaysystem.com.br>
2013-02-16 04:15:46 -0200

    Change splash

--- CustomerManagerJDEJSON/src/br/com/bitwaysystem/activity/Splash.java
+++ CustomerManagerJDEJSON/src/br/com/bitwaysystem/activity/Splash.java
@@ -15,40 +15,40 @@ public class Splash extends Activity implements Runnable {
 
 	// ! ID of the progress dialog.
 	private final int DIALOG_PROGRESS = 1;
-	
+
 	@Override
 	protected Dialog onCreateDialog(int id) {
 		if (id == DIALOG_PROGRESS) {
-			// String message = getString(R.string.consulta_btn);
-			ProgressDialog dialog = new ProgressDialog(this);
-			dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);			
-			dialog.setMessage("Carregando");
-			dialog.setCancelable(false);
+
+			ProgressDialog dialog = ProgressDialog.show(Splash.this,
+					"Bem vindo...", "Carregando, por favor aguarde...", false,
+					false);
 			return dialog;
 		} else {
 			return null;
 		}
-	}	
-	
-	public void onCreate(Bundle savedInstanceState) {
-			
-			//Remove title bar
-			this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-
-			//Remove notification bar
-			this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-			
-			showDialog(DIALOG_PROGRESS);
-			
-			super.onCreate(savedInstanceState);
-			setContentView(R.layout.splash);
-
-			Handler handler = new Handler();
-			handler.postDelayed(this, 2000);
-		}
-
-		public void run(){
-			startActivity(new Intent(this, GetCustomerCreditInfo.class));
-			finish();
-		}
 	}
+
+	public void onCreate(Bundle savedInstanceState) {
+
+		// Remove title bar
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+		// Remove notification bar
+		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+		showDialog(DIALOG_PROGRESS);
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.splash);
+
+		Handler handler = new Handler();
+		handler.postDelayed(this, 2000);
+	}
+
+	public void run() {
+		startActivity(new Intent(this, GetCustomerCreditInfo.class));
+		finish();
+	}
+}
