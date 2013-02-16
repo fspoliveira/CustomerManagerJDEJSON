8b65d87fc38ef8a773c968b9c5c6a72bd1805a0c
fspoliveira <fspoliveira@bitwaysystem.com.br>
2013-02-16 03:40:51 -0200

    header and footer color change

--- CustomerManagerJDEJSON/gen/com/example/customermanagerjdejson/R.java
+++ CustomerManagerJDEJSON/gen/com/example/customermanagerjdejson/R.java
@@ -12,6 +12,7 @@ public final class R {
     }
     public static final class color {
         public static final int black=0x7f040000;
+        public static final int snow=0x7f040001;
     }
     public static final class drawable {
         public static final int ic_action_search=0x7f020000;
@@ -21,14 +22,16 @@ public final class R {
         public static final int TextView01=0x7f080004;
         public static final int TextView02=0x7f080005;
         public static final int button1=0x7f080002;
-        public static final int iSair=0x7f08000f;
-        public static final int lbl_CNPJ=0x7f08000d;
-        public static final int lbl_Credito=0x7f08000c;
-        public static final int lbl_PedidoAberto=0x7f08000b;
-        public static final int lbl_idCliente=0x7f08000e;
+        public static final int cabecalho=0x7f08000b;
+        public static final int iSair=0x7f080011;
+        public static final int lbl_CNPJ=0x7f08000f;
+        public static final int lbl_Credito=0x7f08000e;
+        public static final int lbl_PedidoAberto=0x7f08000d;
+        public static final int lbl_idCliente=0x7f080010;
         public static final int rodape=0x7f080000;
         public static final int textView1=0x7f080003;
         public static final int textView2=0x7f08000a;
+        public static final int texto1=0x7f08000c;
         public static final int texto2=0x7f080001;
         public static final int txt_CNPJCPF=0x7f080006;
         public static final int txt_Limite=0x7f080008;
@@ -43,6 +46,7 @@ public final class R {
         public static final int down_menu=0x7f070000;
     }
     public static final class string {
+        public static final int OK=0x7f05000b;
         public static final int app_name=0x7f050000;
         public static final int consulta_btn=0x7f050006;
         public static final int limiteCredito=0x7f050005;
@@ -53,6 +57,7 @@ public final class R {
         public static final int title_activity_main=0x7f050002;
         public static final int txtCNPJCPF=0x7f050004;
         public static final int txtCliente=0x7f050003;
+        public static final int txtHeader=0x7f05000a;
     }
     public static final class style {
         public static final int AppTheme=0x7f060000;
